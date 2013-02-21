4172697bc600abe11a5de01d8d7d91b268ac6e2f
lee <ljm@it-innovation.soton.ac.uk>
2013-02-21 16:44:02 +0000

    more complete parsing

--- tokenise.py
+++ tokenise.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# _*_ coding: utf-8 _*_
 
 """
 simple twitter parser
@@ -10,21 +11,42 @@ class Tokenise:
     split the tweet up. idea is to generate a list of words in order.
     extensions could include additional information like punctuation
     """
+    EP = r"""['"“”‘’<>«»{}(\)[\]:]"""
+    NOT_EP = r"""[a-zA-Z0-9]""" 
+    SM_LEFT = r"""([:=]|[;])(|o|O|-)([pP]|[doO]|[D\)\]]|[\(\[])"""
+    SM_RIGHT = r"""([pP]|[doO]|[\)\]]|[\(\[])(|o|O|-)([:=]|[;])"""
+    
+    
+    EP_LEFT_RE = re.compile(r"""(\s|^)(%s+)(%s)""" % (EP, NOT_EP))
+    EP_RIGHT_RE = re.compile(r"""(%s)(%s+)(\s|$)""" % (NOT_EP, EP))
+    WS_RE = re.compile(r'\s+')
+    PROTECT_RE = re.compile('('+SM_LEFT+'|'+SM_RIGHT+')')
     
     def __init__(self):
         self.words = []
-        self.ws_re = re.compile(r'\s+')
         
         
     def tokenise(self, text):
         """
         main parsing function
         """
-        self.words = []
         # remove whitespace
-        text = self.ws_re.sub(' ', text)
-        self.words = text.split()
+        text = self.WS_RE.sub(' ', text)
+        # isolate punctuation from words
+        text = self.EP_LEFT_RE.sub(r"\1\2 \3", text)
+        text = self.EP_RIGHT_RE.sub(r"\1 \2\3", text)
+        # protect any reserved sequences
+        self.words = []
+        pos = 0
+        if self.PROTECT_RE.search(text):
+            for mi in self.PROTECT_RE.finditer(text):
+                self.words.extend(text[pos:mi.start()].split())
+                self.words.append(text[mi.start():mi.end()])
+                pos = mi.end()
+        else:
+            self.words = text.split()
         #
+        print self.words
         return self.words
         
     def words(self):
@@ -33,40 +55,15 @@ class Tokenise:
 
 
 if __name__ == '__main__':
-    #import pymongo
-    #p = Tokenise()
-    #connection = pymongo.connection.Connection()
-    #db = connection.tweets
-    #tweets = db['earthquake']
-    #for ti in tweets.find():
-    #    text = ti['text']
-    #    nuc = len([bi for bi in text if ord(bi)<=255])
-    #    nas = len([bi for bi in text])
-    #    if nuc>nas-5:
-    #        print text, p.tokenise(text)
-    #connection.close()
-    def regex_or(*items):
-        r = '|'.join(items)
-        r = '(' + r + ')'
-        return r
-    def pos_lookahead(r):
-        return '(?=' + r + ')'
-    def neg_lookahead(r):
-        return '(?!' + r + ')'
-    def optional(r):
-        return '(%s)?' % r
-    
-    PunctChars = r'''['".?!,:;]'''
-    Entity = '&(amp|lt|gt|quot);'
-    UrlStart1 = regex_or('https?://', r'www\.')
-    CommonTLDs = regex_or('com','co\\.uk','org','net','info','ca')
-    UrlStart2 = r'[a-z0-9\.-]+?' + r'\.' + CommonTLDs + pos_lookahead(r'[/ \W\b]')
-    UrlBody = r'[^ \t\r\n<>]*?'  # * not + for case of:  "go to bla.com." -- don't want period
-    UrlExtraCrapBeforeEnd = '%s+?' % regex_or(PunctChars, Entity)
-    UrlEnd = regex_or( r'\.\.+', r'[<>]', r'\s', '$')
-    Url = (r'\b' + 
-        regex_or(UrlStart1, UrlStart2) + 
-        UrlBody + 
-        pos_lookahead( optional(UrlExtraCrapBeforeEnd) + UrlEnd))
-    print '(%s)'% Url
-    
+    import pymongo
+    p = Tokenise()
+    connection = pymongo.connection.Connection()
+    db = connection.tweets
+    ti = 0
+    tweets = db['earthquake']
+    for ti in tweets.find():
+        text = ti['text']
+        if ti==10:
+            break
+        ti += 1
+    connection.close()
