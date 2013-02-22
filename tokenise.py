3dfb258edb89a225725845d036721b2b00c4636c
lee <ljm@it-innovation.soton.ac.uk>
2013-02-22 10:53:41 +0000

    issues with url parsing in tweets

--- tokenise.py
+++ tokenise.py
@@ -13,14 +13,25 @@ class Tokenise:
     """
     EP = r"""['"“”‘’<>«»{}(\)[\]:]"""
     NOT_EP = r"""[a-zA-Z0-9]""" 
+    PC = r"""['"“”‘’.?!…,:;]"""
+    ENT = r"""&(?:amp|lt|gt|quot);"""
     SM_LEFT = r"""([:=]|[;])(|o|O|-)([pP]|[doO]|[D\)\]]|[\(\[])"""
     SM_RIGHT = r"""([pP]|[doO]|[\)\]]|[\(\[])(|o|O|-)([:=]|[;])"""
+    #URLS = r"""(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))"""
+    URLS1  = r"""(?:https?://|\bwww\.)"""
+    CTLD = r"""(?:com|org|edu|gov|net|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|pro|tel|travel|xxx)"""
+    CCTLD = r"""(?:ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|ss|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|za|zm|zw)"""
+    URLS2 = r"""\b(?:[A-Za-z\d-])+(?:\.[A-Za-z0-9]+){0,3}\.""" + """(?:"""+CTLD+"""|"""+CCTLD+""")"""+"""(?:\."""+CCTLD+""")?(?=\W|$)"""
+    URLB = r"""(?:[^\.\s<>][^\s<>]*?)?"""
+    URLEC = r"""(?:"""+PC+"""|"""+ENT+""")+?"""
+    URLE = r"""(?:\.\.+|[<>]|\s|$)"""
+    URLS = r"""(?:"""+URLS1+"""|"""+URLS2+""")"""+URLB+"""(?=(?:"""+URLEC+""")?"""+URLE+""")"""
     
     
     EP_LEFT_RE = re.compile(r"""(\s|^)(%s+)(%s)""" % (EP, NOT_EP))
     EP_RIGHT_RE = re.compile(r"""(%s)(%s+)(\s|$)""" % (NOT_EP, EP))
     WS_RE = re.compile(r'\s+')
-    PROTECT_RE = re.compile('('+SM_LEFT+'|'+SM_RIGHT+')')
+    PROTECT_RE = re.compile('('+SM_LEFT+'|'+SM_RIGHT+'|'+URLS+')')
     
     def __init__(self):
         self.words = []
@@ -43,10 +54,11 @@ class Tokenise:
                 self.words.extend(text[pos:mi.start()].split())
                 self.words.append(text[mi.start():mi.end()])
                 pos = mi.end()
+            self.words.extend(text[pos:len(text)].split())
         else:
             self.words = text.split()
         #
-        print self.words
+        #print self.words
         return self.words
         
     def words(self):
@@ -59,11 +71,15 @@ if __name__ == '__main__':
     p = Tokenise()
     connection = pymongo.connection.Connection()
     db = connection.tweets
-    ti = 0
-    tweets = db['earthquake']
+    tind = 0
+    tweets = db['flood']
     for ti in tweets.find():
-        text = ti['text']
-        if ti==10:
-            break
-        ti += 1
+        #text = ti['text']
+        urls = ti['entities']['urls']
+        if urls:
+            for ui in urls:
+                print (u''+ui['url']).encode('utf-8')
+        #if tind==10:
+        #    break
+        tind += 1
     connection.close()
--- tokenise_test.py
+++ tokenise_test.py
@@ -18,21 +18,27 @@ def test_generator(a,b):
     def test(self):
         self.assertEqual(self.tok.tokenise(a), b)
     return test
-
-
-if __name__ == '__main__':
-    # generate tests directly from file
+    
+def create_tests(fname):
+    nfname = fname.split('.')[0]
     lineno = 1
-    for line in open('./examples/simple.tweets', 'r'):
+    for line in open('./examples/%s' % fname, 'r'):
         if line: 
             try:
                 j = json.loads(line)
-                test_name = 'test_simple_%d'%lineno
+                test_name = 'test_%s_%d' % (nfname, lineno)
                 test = test_generator(j['t'], j['r']) 
                 setattr(TestTokenise, test_name, test)
             except: # error in json
                 print 'error : ', line
             lineno += 1
+
+
+if __name__ == '__main__':
+    # generate tests directly from file
+    files = ['urls.tweets']
+    for fi in files:
+        create_tests(fi)
     # run tests
     unittest.main()
     #suite = unittest.TestLoader().loadTestsFromTestCase(TestTokenise)
